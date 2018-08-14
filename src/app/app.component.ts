c37faa352e683a533176297fb2f26cda57ac964f
12jikan <jareem.e.hoff@gmail.com>
2018-08-14 02:56:59 -0400

    set up everything perfectly, so this is pretty much a bomb template if you need help understanding how to setup the live database in firebase. I will double check to see if this is any different from the cloud version, if so I amy just make another github repo with that one too... honestly this is for anyone who needs it, but mostly for me haha ... I'm defintely the only really reading these commits...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+
+// tslint:disable-next-line:max-line-length
+// make sure that you import the angularfire database right here into the component that you're using. Normally, or when keeping best practice in your mind, you would not usually put this in the app.component. You would definitely put this in it's own component where it will be used.
 import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
@@ -8,11 +11,21 @@ import { AngularFireDatabase } from 'angularfire2/database';
 })
 
 export class AppComponent {
+
+  // initalize courses array here vvv
   courses: any[];
 
+
+  // here you want to make sure that you're injecting the AngularFireDatabase that you imported
   constructor(db: AngularFireDatabase ) {
+
+    // using db.list() to get the obeservable and subscribe to courses collection
     db.list('/courses').valueChanges().subscribe( courses => {
+
+      // sets to this.courses pretty obvious in itself no explanation really needed
       this.courses = courses;
+
+      // again just logs it, nothing crazy going on here
       console.log(this.courses);
     });
   }
